576a653f15498c88371a38227504dcb53aebd85a
yusuf b <ykbilgin61@gmail.com>
2021-03-16 18:06:02 +0100

    new layout
    Signed-off-by: yusuf b <ykbilgin61@gmail.com>

--- wallhack.py
+++ wallhack.py
@@ -1,97 +1,85 @@
-# -*- coding: utf-8 -*-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 import pymem
 import time
 import re
 
 
-class Ui_widget(object):
-    def setupUi(self, widget):
-        widget.setObjectName("widget")
-        widget.resize(350, 250)
-        widget.setMinimumSize(QtCore.QSize(350, 250))
-        widget.setMaximumSize(QtCore.QSize(350, 250))
-        widget.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        widget.setMouseTracking(False)
-        widget.setWindowTitle("CS:GO Wallhack")
-        widget.setLocale(QtCore.QLocale(QtCore.QLocale.Turkish, QtCore.QLocale.Turkey))
-        self.stop_button = QtWidgets.QPushButton(widget)
-        self.stop_button.setGeometry(QtCore.QRect(170, 120, 131, 41))
-        self.stop_button.setObjectName("stop_button")
-        self.progressBar = QtWidgets.QProgressBar(widget)
-        self.progressBar.setEnabled(True)
-        self.progressBar.setGeometry(QtCore.QRect(20, 90, 281, 21))
-        self.progressBar.setProperty("value", 0)
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(637, 339)
+        self.groupBox = QtWidgets.QGroupBox(Form)
+        self.groupBox.setGeometry(QtCore.QRect(20, 240, 241, 91))
+        self.groupBox.setObjectName("groupBox")
+        self.pushButton = QtWidgets.QPushButton(self.groupBox)
+        self.pushButton.setGeometry(QtCore.QRect(10, 40, 93, 31))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.groupBox)
+        self.pushButton_2.setGeometry(QtCore.QRect(120, 40, 93, 28))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.groupBox_2 = QtWidgets.QGroupBox(Form)
+        self.groupBox_2.setGeometry(QtCore.QRect(280, 240, 241, 91))
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.groupBox_2)
+        self.pushButton_3.setGeometry(QtCore.QRect(10, 40, 93, 28))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(self.groupBox_2)
+        self.pushButton_4.setGeometry(QtCore.QRect(120, 40, 93, 28))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.groupBox_3 = QtWidgets.QGroupBox(Form)
+        self.groupBox_3.setGeometry(QtCore.QRect(20, 150, 201, 80))
+        self.groupBox_3.setObjectName("groupBox_3")
+        self.pushButton_5 = QtWidgets.QPushButton(self.groupBox_3)
+        self.pushButton_5.setGeometry(QtCore.QRect(10, 30, 171, 28))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.imageLabel = QtWidgets.QLabel(Form)
+        self.imageLabel.setGeometry(QtCore.QRect(240, 10, 390, 219))
+        self.pixmap = QtGui.QPixmap("img/ekremabi.png")
+        self.imageLabel.setPixmap(self.pixmap)
+        self.progressBar = QtWidgets.QProgressBar(Form)
+        self.progressBar.setGeometry(QtCore.QRect(20, 100, 201, 23))
+        self.progressBar.setProperty("value", 24)
         self.progressBar.setObjectName("progressBar")
-        self.label = QtWidgets.QLabel(widget)
-        self.label.setGeometry(QtCore.QRect(20, 10, 141, 21))
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(540, 310, 91, 21))
         self.label.setObjectName("label")
-        self.run_button = QtWidgets.QPushButton(widget)
-        self.run_button.setGeometry(QtCore.QRect(20, 120, 131, 41))
-        self.run_button.setObjectName("run_button")
-        self.label2 = QtWidgets.QLabel(widget)
-        self.label2.setGeometry(QtCore.QRect(20, 50, 171, 21))
-        self.label2.setText("")
-        self.label2.setObjectName("label2")
-        self.label_2 = QtWidgets.QLabel(widget)
-        self.label_2.setGeometry(QtCore.QRect(10, 180, 331, 21))
-        self.label_2.setLineWidth(1)
+        self.line = QtWidgets.QFrame(Form)
+        self.line.setGeometry(QtCore.QRect(20, 130, 201, 16))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.line_2 = QtWidgets.QFrame(Form)
+        self.line_2.setGeometry(QtCore.QRect(20, 70, 201, 16))
+        self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_2.setObjectName("line_2")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(20, 30, 161, 21))
         self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(widget)
-        self.label_3.setGeometry(QtCore.QRect(40, 200, 291, 16))
-        self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(widget)
-        self.label_4.setGeometry(QtCore.QRect(40, 220, 281, 16))
-        self.label_4.setObjectName("label_4")
 
-        self.retranslateUi(widget)
-        QtCore.QMetaObject.connectSlotsByName(widget)
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
 
-        self.run_button.clicked.connect(lambda: self.injectScript(2))
-        self.stop_button.clicked.connect(lambda: self.injectScript(1))
-
-    def retranslateUi(self, widget):
+    def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        self.stop_button.setText(_translate("widget", "Durdur"))
-        self.label.setText(_translate("widget", "CS:GO wallhack hilesi"))
-        self.run_button.setText(_translate("widget", "Çalıştır"))
-        self.label_2.setText(_translate("widget", "Not: Hileyi durdurduktan sonra bağzı sistemlerde oyun "))
-        self.label_3.setText(_translate("widget", "çökebilir. Oyunu yeniden başlattığınız zaman "))
-        self.label_4.setText(_translate("widget", "herşey normal şekilde çalışacaktır."))
+        Form.setWindowTitle(_translate("Form", "CS:GO Wallhack"))
+        self.groupBox.setTitle(_translate("Form", "Bizim Takım"))
+        self.pushButton.setText(_translate("Form", "fosforlu"))
+        self.pushButton_2.setText(_translate("Form", "Normal"))
+        self.groupBox_2.setTitle(_translate("Form", "Düşman Takım"))
+        self.pushButton_3.setText(_translate("Form", "fosforlu"))
+        self.pushButton_4.setText(_translate("Form", "Normal"))
+        self.groupBox_3.setTitle(_translate("Form", "Herkez"))
+        self.pushButton_5.setText(_translate("Form", "Herkezi saydam mavi yap"))
+        self.label.setText(_translate("Form", "© 2021 | v0.2"))
+        self.label_2.setText(_translate("Form", "Hile enjekte edildi"))
 
-    def injectScript(self, x):
-        try:
-            pm = pymem.Pymem('csgo.exe')
-            client = pymem.process.module_from_name(pm.process_handle,
-                                                    'client.dll')
-            clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
-            address = client.lpBaseOfDll + re.search(rb'\x83\xF8.\x8B\x45\x08\x0F',
-                                                     clientModule).start() + x
-            pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
-            pm.close_process()
-            self.load_animation()
-            if x == 2:
-                self.label2.setText("Hile enjekte edildi")
-            elif x == 1:
-                self.label2.setText("Hile durduruldu")
-        except:
-            self.label2.setText("Görünen o ki csgo çalışmıyor")
-        
-
-    def load_animation(self):
-        count = 0
-        while count < 100:
-            count += 5
-            time.sleep(0.1)
-            self.progressBar.setValue(count)
-    
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    widget = QtWidgets.QWidget()
-    ui = Ui_widget()
-    ui.setupUi(widget)
-    widget.show()
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
     sys.exit(app.exec_())
