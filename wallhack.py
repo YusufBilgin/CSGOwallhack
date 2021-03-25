391ed16c604da38a3c554301c396ef73bcf682ae
yusuf b <ykbilgin61@gmail.com>
2021-03-25 22:51:59 +0100

    added multithreading and wh improved
    Signed-off-by: yusuf b <ykbilgin61@gmail.com>

--- wallhack.py
+++ wallhack.py
@@ -1,139 +1,161 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import (
+    QObject,
+    QThread,
+    pyqtSignal,
+    QRect,
+    QSize,
+    Qt
+)
+from PyQt5.QtWidgets import (
+    QProgressBar,
+    QApplication,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+    QGroupBox,
+    QFrame
+)
+from PyQt5.QtGui import QPixmap
 import pymem.process
 import keyboard
 import pymem
-import time
 import sys
 import re
 
-dwEntityList = (0x4DA2F24)
-dwLocalPlayer = (0xD8B2DC)
+# csgo offsets
+dwEntityList = (0x4DA3F44)
+dwLocalPlayer = (0xD8C2CC)
 m_iTeamNum = (0xF4)
-dwGlowObjectManager = (0x52EB518)
+dwGlowObjectManager = (0x52EC558)
 m_iGlowIndex = (0xA438)
 
 
-class Ui_widget(object):
-    def setupUi(self, widget):
-        widget.setObjectName("widget")
-        widget.resize(637, 339)
-        widget.setMinimumSize(QtCore.QSize(637, 339))
-        widget.setMaximumSize(QtCore.QSize(637, 339))
-        self.groupBox = QtWidgets.QGroupBox(widget)
-        self.groupBox.setGeometry(QtCore.QRect(20, 240, 241, 91))
+# worker class
+class Worker(QObject):
+    finished = pyqtSignal()
+    progress = pyqtSignal(int)
+
+    def __init__(self, parent = None):
+        QObject.__init__(self, parent = parent)
+        self.continue_run = True
+
+    def run(self):
+        pm = pymem.Pymem("csgo.exe")
+        client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
+
+        while self.continue_run:
+            glow_manager = pm.read_int(client + dwGlowObjectManager)
+
+            for i in range(1, 32):
+                entity = pm.read_int(client + dwEntityList + i * 0x10)
+
+                if entity:
+                    entity_team_id = pm.read_int(entity + m_iTeamNum)
+                    entity_glow = pm.read_int(entity + m_iGlowIndex)
+                    if entity_team_id == 2:
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(1))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(0))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))
+                        pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)
+        self.finished.emit()
+
+
+class Window(QMainWindow):
+
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.setupUi()  # Creates all required graphical components
+
+    def setupUi(self):
+
+        # Window
+        self.setWindowTitle("CS:GO Wallhack")
+        self.resize(637, 339)
+        self.setMinimumSize(QSize(637, 339))
+        self.setMaximumSize(QSize(637, 339))
+
+        # create widgets
+        self.groupBox = QGroupBox("Bizim Takım", self)
+        self.groupBox.setGeometry(QRect(20, 240, 241, 91))
         self.groupBox.setObjectName("groupBox")
-        self.pushButton = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton.setGeometry(QtCore.QRect(10, 40, 93, 31))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton_2.setGeometry(QtCore.QRect(120, 40, 93, 28))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.groupBox_2 = QtWidgets.QGroupBox(widget)
-        self.groupBox_2.setGeometry(QtCore.QRect(280, 240, 241, 91))
+        self.counterEnableButton = QPushButton("fosforlu", self.groupBox)
+        self.counterEnableButton.setGeometry(QRect(10, 40, 93, 31))
+        self.counterEnableButton.setObjectName("pushButton")
+        self.counterDisableButton = QPushButton("Normal", self.groupBox)
+        self.counterDisableButton.setGeometry(QRect(120, 40, 93, 28))
+        self.counterDisableButton.setObjectName("pushButton_2")
+        self.groupBox_2 = QGroupBox("Düşman Takım", self)
+        self.groupBox_2.setGeometry(QRect(280, 240, 241, 91))
         self.groupBox_2.setObjectName("groupBox_2")
-        self.pushButton_3 = QtWidgets.QPushButton(self.groupBox_2)
-        self.pushButton_3.setGeometry(QtCore.QRect(10, 40, 93, 28))
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(self.groupBox_2)
-        self.pushButton_4.setGeometry(QtCore.QRect(120, 40, 93, 28))
-        self.pushButton_4.setObjectName("pushButton_4")
-        self.groupBox_3 = QtWidgets.QGroupBox(widget)
-        self.groupBox_3.setGeometry(QtCore.QRect(20, 150, 201, 80))
+        self.terroristEnableButton = QPushButton("fosforlu", self.groupBox_2)
+        self.terroristEnableButton.setGeometry(QRect(10, 40, 93, 28))
+        self.terroristEnableButton.setObjectName("pushButton_3")
+        self.terroristDisableButton = QPushButton("Normal", self.groupBox_2)
+        self.terroristDisableButton.setGeometry(QRect(120, 40, 93, 28))
+        self.terroristDisableButton.setObjectName("pushButton_4")
+        self.groupBox_3 = QGroupBox("Herkez", self)
+        self.groupBox_3.setGeometry(QRect(20, 150, 201, 80))
         self.groupBox_3.setObjectName("groupBox_3")
-        self.pushButton_5 = QtWidgets.QPushButton(self.groupBox_3)
-        self.pushButton_5.setGeometry(QtCore.QRect(10, 30, 171, 28))
+        self.pushButton_5 = QPushButton("Herkezi saydam mavi yap", self.groupBox_3)
+        self.pushButton_5.setGeometry(QRect(10, 30, 171, 28))
         self.pushButton_5.setObjectName("pushButton_5")
-        self.imageLabel = QtWidgets.QLabel(widget)
-        self.imageLabel.setGeometry(QtCore.QRect(240, 10, 390, 219))
-        self.pixmap = QtGui.QPixmap("img/ekremabi.png")
+        self.imageLabel = QLabel("© 2021 | v0.2", self)
+        self.imageLabel.setGeometry(QRect(240, 10, 390, 219))
+        self.pixmap = QPixmap("img/ekremabi.png")
         self.imageLabel.setPixmap(self.pixmap)
-        self.progressBar = QtWidgets.QProgressBar(widget)
-        self.progressBar.setGeometry(QtCore.QRect(20, 100, 201, 23))
+        self.progressBar = QProgressBar(self)
+        self.progressBar.setGeometry(QRect(20, 100, 201, 23))
         self.progressBar.setProperty("value", 0)
         self.progressBar.setObjectName("progressBar")
-        self.label = QtWidgets.QLabel(widget)
-        self.label.setGeometry(QtCore.QRect(540, 310, 91, 21))
+        self.label = QLabel(self)
+        self.label.setGeometry(QRect(540, 310, 91, 21))
         self.label.setObjectName("label")
-        self.line = QtWidgets.QFrame(widget)
-        self.line.setGeometry(QtCore.QRect(20, 130, 201, 16))
-        self.line.setFrameShape(QtWidgets.QFrame.HLine)
-        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line = QFrame(self)
+        self.line.setGeometry(QRect(20, 130, 201, 16))
+        self.line.setFrameShape(QFrame.HLine)
+        self.line.setFrameShadow(QFrame.Sunken)
         self.line.setObjectName("line")
-        self.line_2 = QtWidgets.QFrame(widget)
-        self.line_2.setGeometry(QtCore.QRect(20, 70, 201, 16))
-        self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
-        self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_2 = QFrame(self)
+        self.line_2.setGeometry(QRect(20, 70, 201, 16))
+        self.line_2.setFrameShape(QFrame.HLine)
+        self.line_2.setFrameShadow(QFrame.Sunken)
         self.line_2.setObjectName("line_2")
-        self.label_2 = QtWidgets.QLabel(widget)
-        self.label_2.setGeometry(QtCore.QRect(20, 30, 161, 21))
+        self.label_2 = QLabel(self)
+        self.label_2.setGeometry(QRect(20, 30, 161, 21))
         self.label_2.setObjectName("label_2")
 
-        self.retranslateUi(widget)
-        QtCore.QMetaObject.connectSlotsByName(widget)
+        # Connections
+        self.counterEnableButton.clicked.connect(lambda: self.run_script())
+        self.terroristEnableButton.clicked.connect(lambda: self.run_script())
 
-        self.pushButton.clicked.connect(lambda: self.injectScript("enemy", True))
-        self.pushButton_2.clicked.connect(lambda: self.injectScript("enemy", False))
-        self.pushButton_3.clicked.connect(lambda: self.injectScript("counter", True))
-        self.pushButton_4.clicked.connect(lambda: self.injectScript("counter", False))
-        self.pushButton_5.clicked.connect(lambda: self.injectScript("all", True))
+    def run_script(self):
+        self.thread = QThread()
+        self.worker = Worker()
+        self.worker.moveToThread(self.thread)
 
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
 
-    def injectScript(self, team, value):
-        pm = pymem.Pymem("csgo.exe")
-        client = pymem.process.module_from_name(pm.process_handle,
-                                                "client.dll").lpBaseOfDll
-
-        if team == "all":
-            try:
-                clientModule = pm.read_bytes(client, client.SizeOfImage)
-                address = client + re.search(rb'\x83\xF8.\x8B\x45\x08\x0F',
-                                            clientModule).start() + x
-
-                pm.write_uchar(address, 2 if pm.read_uchar(address) == 1 else 1)
-                pm.close_process()
-            except:
-                pass
-        else:
-            while True:
-                glow_manager = pm.read_int(client + dwGlowObjectManager)
-
-                for i in range(1, 32):
-                    entity = pm.read_int(client + dwEntityList + i * 0x10)
-
-                    if entity:
-                        entity_team_id = pm-read_int(entity + m_iTeamNum)
-                        entity_glow = pm.read_int(entity + m_iGlowIndex)
+        self.thread.start()
+        self.counterDisableButton.clicked.connect(self.stop_thread)
 
 
-        if team == "enemy":
-            pass
-        elif team == "counter":
-            pass
-        elif team == "all":
-            pass
-        else:
-            pass
+        self.counterEnableButton.setEnabled(False)
+        self.thread.finished.connect(
+            lambda: self.counterDisableButton.setEnabled(True)
+        )
+
+    def stop_thread(self):
+        print("trying to stop")
+        self.worker.continue_run = False
 
 
-    def retranslateUi(self, widget):
-        _translate = QtCore.QCoreApplication.translate
-        widget.setWindowTitle(_translate("widget", "CS:GO Wallhack"))
-        self.groupBox.setTitle(_translate("widget", "Bizim Takım"))
-        self.pushButton.setText(_translate("widget", "fosforlu"))
-        self.pushButton_2.setText(_translate("widget", "Normal"))
-        self.groupBox_2.setTitle(_translate("widget", "Düşman Takım"))
-        self.pushButton_3.setText(_translate("widget", "fosforlu"))
-        self.pushButton_4.setText(_translate("widget", "Normal"))
-        self.groupBox_3.setTitle(_translate("widget", "Herkez"))
-        self.pushButton_5.setText(_translate("widget", "Herkezi saydam mavi yap"))
-        self.label.setText(_translate("widget", "© 2021 | v0.2"))
-        self.label_2.setText(_translate("widget", "Hile enjekte edildi"))
-
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    widget = QtWidgets.QWidget()
-    ui = Ui_widget()
-    ui.setupUi(widget)
-    widget.show()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+win = Window()
+win.show()
+sys.exit(app.exec_())
